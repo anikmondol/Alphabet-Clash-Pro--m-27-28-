2f51b4d6295126be5eead5d01a1299c27694c79f
anikmondol <anikmondol558363@gmil.com>
2024-04-04 19:34:56 +0600

    module-27 done with full Practice

--- js/index.js
+++ js/index.js
@@ -1,6 +1,28 @@
+function continueGame(){
+// step -1: generate a random alphabet
+const alphabet =  getARandomAlphabet();
 
+
+// set randomly generated alphabet to the screen (show it)
+const currentAlphabet = document.getElementById('current_alphabet');
+currentAlphabet.innerText = alphabet;
+
+
+// set background color
+setBackgroundColorById(alphabet);
+
+
+}
 
 function play(){
-    setClassList('home');
-    removeClassList('play_ground')
+    // step-1: hide the home screen
+    hiddenElementById('home_screen');
+
+    // show the playground
+    showElementById('play_ground')
+
+    // continueGame function
+    continueGame();
 }
+
+
--- js/utility.js
+++ js/utility.js
@@ -1,10 +1,35 @@
-function setClassList(elementId){
+function hiddenElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 }
 
 
-function removeClassList(elementId){
+function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden')
+}
+
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-[#FFA500]')
+}
+
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-[#FFA500]')
+}
+
+function getARandomAlphabet(){
+    // get or create an alphabet array
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    
+    // get a random index between 0 - 25
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+   
+
+    const alphabet = alphabets[index];
+    return alphabet;
 }
